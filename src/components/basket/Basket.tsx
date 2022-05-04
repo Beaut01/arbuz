534126ce5509d19deea1e672f3e7ded4ac985669
e.korovin <e.korovin@oblakogroup.ru>
2022-05-04 22:57:15 +0700

    тест коммита из вебшторма

--- src/components/basket/Basket.tsx
+++ src/components/basket/Basket.tsx
@@ -5,6 +5,7 @@ import { styles } from './styles'
 export const Basket = () => {
     return(
         <View style={styles.wrapper}>
+            <Text />
             <TouchableOpacity activeOpacity={0.4}> 
                 <Text style={styles.text}>Basket</Text>
             </TouchableOpacity>
